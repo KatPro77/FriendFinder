2c8e00de4b36c0c64297bc354ce61cf3bcd43f6a
Katie Stanley <KStanley@optimaengineering.com>
2018-09-26 17:24:36 -0400

    server file

---
+++ server.js
@@ -0,0 +1,11 @@
+//Dependencies
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+//Express App
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
