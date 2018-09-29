ec33f7a5b01647658c4c9170e490b5e677ccc8a8
Katerina Stanley <katerinastanley77@gmail.com>
2018-09-29 16:09:29 -0400

    html and jQuery

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,18 +1,18 @@
 var friends = [
     {
         name: "Bobby",
-        photo: "https://goo.gl/images/i4L3ou",
-        scores: [5, 2, 3, 4, 4, 1, 3, 4, 1]
+        photo: "http://indeksonline.net/wp-content/uploads/2018/08/Isaac-Carew-hp-GQ-19Oct17_b.jpg",
+        score: [5, 2, 3, 4, 4, 1, 3, 4, 1]
     },
     {
         name: "JT",
-        photo: "https://goo.gl/images/6cuUuv",
-        scores: [2, 5, 1, 2, 3, 5, 2, 4, 1, 5]
+        photo: "http://styloss.com/wp-content/uploads/2015/04/1-1.jpg",
+        score: [2, 5, 1, 2, 3, 5, 2, 4, 1, 5]
     },
     {
         name: "Alberto",
-        photo: "https://goo.gl/images/5cCRJg",
-        scores: [3, 5, 1, 4, 2, 5, 3, 5, 3, 2]
+        photo: "https://i.pinimg.com/736x/d4/8a/d4/d48ad49f68a05063a801c7002c636447.jpg",
+        score: [3, 5, 1, 4, 2, 5, 3, 5, 3, 2]
     }
 ];
 
